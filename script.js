4dbdf4505414b08ea4c1665d11fec630ff4af68d
Mai Doan <49138712+chalalala@users.noreply.github.com>
2023-03-11 01:17:43 +0700

    feat: create toc generator

---
+++ script.js
@@ -0,0 +1,58 @@
+const cleanUpLine = (line) => {
+	const invalidExp = /[^#\w\s]/;
+
+	if (invalidExp.test(line)) {
+		return line.replace(invalidExp, "");
+	}
+
+	return line;
+};
+
+const slugify = (str) =>
+	str
+		.toLowerCase()
+		.trim()
+		.replace(/[^\w\s-]/g, "")
+		.replace(/[\s_-]+/g, "-")
+		.replace(/^-+|-+$/g, "");
+
+const LIST_SYMBOLS = {
+	0: "-",
+	1: "+",
+	2: "*",
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+	const submitButton = document.querySelector("button[type=submit]");
+
+	submitButton.addEventListener("click", (event) => {
+		event.preventDefault();
+
+		const inputValue = document.getElementById("input").value;
+		const output = document.getElementById("output");
+
+		const lines = inputValue.split("\n");
+		const headingExp = /^(#+)\s/gm;
+
+		const tocLines = lines.reduce((headings, line) => {
+			const cleanLine = cleanUpLine(line);
+
+			if (headingExp.test(cleanLine)) {
+				const level = cleanLine.match(/#/g).length - 1;
+				const listSymbol = LIST_SYMBOLS[level % 3];
+				const replacedContent = cleanLine.replace(headingExp, "");
+				const slug = slugify(cleanLine);
+
+				link = `${[...Array(level * 2).keys()]
+					.fill(" ")
+					.join("")}${listSymbol} [${replacedContent}](#${slug})`;
+
+				return [...headings, link];
+			}
+
+			return headings;
+		}, []);
+
+		output.value = tocLines.join("\n");
+	});
+});
